364ce82854fed5eabbbd5387c056aba8d99bfb0c
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2021-08-19 21:36:07 +0200

    Remove unneeded code from Users context and add call to GenServer

--- lib/remote_web/controllers/user_controller.ex
+++ lib/remote_web/controllers/user_controller.ex
@@ -1,12 +1,12 @@
 defmodule RemoteWeb.UserController do
   use RemoteWeb, :controller
 
-  alias Remote.Users.GenServer
+  alias Remote.Users
 
   action_fallback RemoteWeb.FallbackController
 
   def index(conn, _params) do
-    {timestamp, users} = GenServer.get_at_most_two_users_with_more_than_max_points()
+    {timestamp, users} = Users.get_at_most_two_users_with_more_than_max_points()
     render(conn, "index.json", users: users, timestamp: timestamp)
   end
 end
